d873df2448c63903399ea43da85e28df0a2ccc4e
PAQUET_Corentin <propcorentin87@gmail.com>
2025-09-10 13:30:22 +0200

    App : navbar color

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div className="text-center">
       <header>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/">John Doe</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
