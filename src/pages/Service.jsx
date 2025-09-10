a24ea027de48b54459244ada67d5f489d773ca2a
PAQUET_Corentin <propcorentin87@gmail.com>
2025-09-10 16:54:03 +0200

    Service : couleur SVG en cour

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,13 +1,62 @@
 // Images
-import banner from '../asset/img/banner.jpg';
+import brush from '../asset/svg/brush.svg';
+import search from '../asset/svg/search.svg';
+import codeslash from '../asset/svg/code-slash.svg';
 
 // Style
 import '../style/style.css';
+import '../style/Service.css';
 
 export default function Service() {
   return (
     <div className='Service'>
-      <div id='header'>
+      <div id='header'></div>
+
+      <div className='items-align-center justify-content-center text-center my-5 mx-auto'>
+        <div>
+            <h1>Mon offre de services</h1>
+            <p><small>Voici les prestations sur lesquelles je peux intervenir</small></p>
+            <hr class="border border-primary border-2 opacity-100 mx-auto" style={{ width: "35%" }} />
+        </div>
+        
+
+        <div className='container my-5'>
+            <div className='row g-4 justify-content-center'>
+                <div className='col-12 col-sm-6 col-md-4'>
+                    <div className='option-service text-center p-3'>
+                        <img src={brush} alt="UX Design" style={{ width: "50px", height: "50px" }} />
+                        <h2>UX Design</h2>
+                        <p>
+                            L'UX Design est une discipline qui consiste à concevoir des
+                            produits (sites web, applications mobiles, logiciels, objets connectés, etc.)
+                            en plaçant l'utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur la
+                            plus fluide et agréable possible.
+                        </p>
+                    </div>
+                </div>
+                
+                <div className="col-12 col-sm-6 col-md-4">
+                    <div className='option-service text-center p-3'>
+                        <img src={codeslash} alt="Développement Web" style={{ width: "50px", height: "50px" }} />
+                        <h2>Développement Web</h2>
+                        <p>
+                            Le développement de sites web consiste à créer des sites internet en utilisant des langages de programmation (HTML, CSS, JavaScript, PHP, etc.) et des frameworks (Bootstrap, React, Angular, etc.).
+                        </p>
+                    </div>
+                </div>
+                
+                <div className="col-12 col-sm-6 col-md-4">
+                    <div className='option-service text-center p-3'>
+                        <img src={search} alt="Référencement" style={{ width: "50px", height: "50px" }} />
+                        <h2>Référencement</h2>
+                        <p>
+                            Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web pour le faire remonter dans les résultats des moteurs de recherche (Google, Bing, Yahoo, etc.). L'objectif est d'attirer un maximum de visiteurs qualifiés sur le site.
+                        </p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
 
       </div>
     </div>
